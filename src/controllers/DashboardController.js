9a299a7863edd377b7941106e4dad8decd516e8d
Jorge Linhares <jorgehclinhares@gmail.com>
2019-10-10 11:48:11 -0300

    feat: dashboard com lista de sistemas e quantidade de falhas abertas

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -2,14 +2,13 @@ const DashboardRepository = require('../repositories/DashboardRepository')
 
 module.exports = {
   async show(req, res) {
-    let data = { status: 400, success: false, message: 'Email não informado.', data: {} }
-
+    // filter by user
     const projects = await DashboardRepository.show()
 
-    if (projects) {
-      data = { status: 200, success: true, message: '', data: projects }
+    if (!projects) {
+      res.status(404).json({ success: false, message: 'Error', data: {} })
     }
 
-    return res.status(data.status).json(data)
+    return res.status(200).json({ success: true, message: '', data: projects })
   }
 }
--- src/repositories/DashboardRepository.js
+++ src/repositories/DashboardRepository.js
@@ -1,6 +1,39 @@
 const Project = require('../models/Project')
+const Error = require('../models/Error')
 
 module.exports.show = async () => {
-  const projects = Project.find();
+  const projects = Project.aggregate([
+    {
+      $lookup: {
+        from: "errors",
+        let: { projectId: "$_id" },
+        pipeline: [
+          {
+            $match: {
+              $expr: {
+                $eq: ["$project", "$$projectId"]
+              }
+            }
+          },
+          {
+            $match: {
+              resolved: false
+            }
+          }
+        ],
+        as: "errors"
+      }
+    },
+    {
+      $project: {
+        _id: 1,
+        name: 1,
+        errors: {
+          $size: "$errors"
+        }
+      }
+    }
+  ])
+
   return projects
 }
